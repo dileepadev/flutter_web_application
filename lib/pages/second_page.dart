f50b3742ec373f7a447c8719e5ff57034ed5f0be
Dileepa Bandara <80202913+dileepabandara@users.noreply.github.com>
2022-06-18 22:47:44 +0530

    UPGRADE: upgrade to Flutter 3.0

--- lib/pages/second_page.dart
+++ lib/pages/second_page.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 
 class SecondPage extends StatefulWidget {
-  const SecondPage({ Key? key }) : super(key: key);
+  const SecondPage({Key? key}) : super(key: key);
 
   @override
-  _SecondPageState createState() => _SecondPageState();
+  State<SecondPage> createState() => _SecondPageState();
 }
 
 class _SecondPageState extends State<SecondPage> {
